90e95721fc4e230591a6ea84ff83a6afc9b7de5c
knee-rel <nirelmarieibarra@gmail.com>
2021-05-07 10:06:18 +0800

    set-up react native app

---
+++ components/styles.js
@@ -0,0 +1,13 @@
+import styles from 'styled-components';
+
+//Change colors depending on need
+export const Colors = {
+  primary: 'ffffff',
+  secondary: '007070',
+  tertiary: 'FFBD10',
+  text: '2D2D2D',
+  inactive: 'C4C4C4',
+  danger: '#FF8159',
+};
+
+const { primary, secondary, tertiary, text, inactive, danger } = Colors;
